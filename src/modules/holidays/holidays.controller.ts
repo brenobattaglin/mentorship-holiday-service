f9a5ca21566be3be7f90f899fe5a9688c2eea6ec
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2026-03-10 16:15:19 -0300

    feat: implement service and controller for brazil holidays

--- src/modules/holidays/holidays.controller.ts
+++ src/modules/holidays/holidays.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { HolidayResponseDto } from './dto/holiday-response.dto';
 import { HolidaysService } from './holidays.service';
 
 @ApiTags('holidays')
@@ -8,8 +9,16 @@ export class HolidaysController {
   constructor(private readonly holidaysService: HolidaysService) {}
 
   @Get('brazil')
-  getBrazilHolidays() {}
+  @ApiOperation({ summary: 'Get public holidays for Brazil this year' })
+  @ApiOkResponse({ type: HolidayResponseDto, isArray: true })
+  getBrazilHolidays(): Promise<HolidayResponseDto[]> {
+    return this.holidaysService.getBrazilHolidays();
+  }
 
   @Get('brazil/upcoming')
-  getUpcomingBrazilHolidays() {}
+  @ApiOperation({ summary: 'Get upcoming public holidays for Brazil' })
+  @ApiOkResponse({ type: HolidayResponseDto, isArray: true })
+  getUpcomingBrazilHolidays(): Promise<HolidayResponseDto[]> {
+    return this.holidaysService.getUpcomingBrazilHolidays();
+  }
 }
--- src/modules/holidays/holidays.service.ts
+++ src/modules/holidays/holidays.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { HolidaysAdapter } from './holidays.adapter';
+import { HolidayResponseDto } from './dto/holiday-response.dto';
 
 @Injectable()
-export class HolidaysService {}
+export class HolidaysService {
+  constructor(private readonly holidaysAdapter: HolidaysAdapter) {}
+
+  async getBrazilHolidays(): Promise<HolidayResponseDto[]> {
+    const year = new Date().getFullYear();
+    const holidays = await this.holidaysAdapter.getPublicHolidays(year, 'BR');
+
+    return holidays.map((h) => ({
+      date: h.date,
+      name: h.name,
+      localName: h.localName,
+      types: h.types,
+    }));
+  }
+
+  async getUpcomingBrazilHolidays(): Promise<HolidayResponseDto[]> {
+    const holidays = await this.getBrazilHolidays();
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return holidays.filter((h) => new Date(h.date) >= today);
+  }
+}
